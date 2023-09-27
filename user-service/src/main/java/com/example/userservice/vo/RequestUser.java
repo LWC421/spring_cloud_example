f47d04bcc88cae23c8b62fe677727f0ba2424df5
LWC421 <lwc421@gmail.com>
2023-09-27 20:13:01 +0900

    유저서비스 회원가입 완성

--- user-service/src/main/java/com/example/userservice/vo/RequestUser.java
+++ user-service/src/main/java/com/example/userservice/vo/RequestUser.java
@@ -3,12 +3,20 @@ package com.example.userservice.vo;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class RequestUser {
 
-  @NotNull(message = "")
+  @NotNull(message = "Email is Null")
+  @Size(min = 2, message = "Email more then 2")
   private String email;
+  
+  @NotNull(message = "Name is Null")
+  @Size(min = 2, message = "Name more then 2")
   private String name;
+  
+  @NotNull(message = "Password is Null")
+  @Size(min = 8, message = "Password more then 8")
   private String pwd;
 }
