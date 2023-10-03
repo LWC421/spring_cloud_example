7b1bc47605b655ab0a9afb4cb66f08298b11655f
LWC421 <lwc421@gmail.com>
2023-10-03 13:39:17 +0900

    카프카 적용중

---
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducer.java
@@ -0,0 +1,17 @@
+package com.example.orderservice.messagequeue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducer {
+  private KafkaTemplate<String, String> kafkaTemplate;
+  
+  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+  
+  
+}
