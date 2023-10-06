90c1760cce2b16fdc971fcdcdc47dfabf22d808f
LWC421 <lwc421@gmail.com>
2023-10-06 17:57:33 +0900

    DB Mysql로 변경

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,20 +1,28 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.service.OrderService;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order-service")
 public class OrderController {
   
   Environment env;
+  OrderService orderService;
+  KafkaProducer kafkaProducer;
   
-  public OrderController(Environment env){
+  public OrderController(Environment env,
+                         OrderService orderService,
+                         KafkaProducer kafkaProducer) {
     this.env = env;
+    this.orderService = orderService;
+    this.kafkaProducer = kafkaProducer;
   }
   
   @GetMapping("/health_check")
@@ -22,11 +30,24 @@ public class OrderController {
     return String.format("Catalog Service PORT : %s", env.getProperty("local.server.port"));
   }
 
+  @PostMapping("/{userId}/orders")
+  public ResponseEntity createOrder(@PathVariable("userId") String userId){
+    
+    /* 데이터 만들기 생략 */
+    
+    OrderDto orderDto = orderService.createOrder();
+    
+    
+    kafkaProducer.send("example-catalog-topic", orderDto);
+    
+    return ResponseEntity.status(HttpStatus.CREATED).body(null);
+  }
+  
   @GetMapping("/{userId}/orders")
   public ResponseEntity<String> getOrder(@PathVariable("userId") String userId){
     
     return ResponseEntity.ok(userId);
   }
-
+  
 }
 
--- order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducer.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducer.java
@@ -1,5 +1,8 @@
 package com.example.orderservice.messagequeue;
 
+import com.example.orderservice.dto.OrderDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -13,5 +16,17 @@ public class KafkaProducer {
     this.kafkaTemplate = kafkaTemplate;
   }
   
-  
+  public OrderDto send(String topic, OrderDto orderDto){
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonInString = "";
+    try{
+      jsonInString =  mapper.writeValueAsString(orderDto);
+    } catch(JsonProcessingException e){
+      e.printStackTrace();
+    }
+    
+    kafkaTemplate.send(topic, jsonInString);  //실제로 카프카로 데이터를 전송
+    
+    return orderDto;
+  }
 }
